7db4e78d2937bf616684b423e3f08f1dad9e3bed
Yuber Vasquez <yuber.vasquez@gmail.com>
2023-08-15 09:05:11 -0500

    antes de modificar gitignore porque me apare con cambios

--- lib/app/domain/models/user.dart
+++ lib/app/domain/models/user.dart
@@ -1,17 +1,24 @@
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'user.g.dart';
+
+@JsonSerializable()
 class User extends Equatable {
   const User({required this.id, required this.username});
-  factory User.fromJson(Map<String, dynamic> json) => User(
-        id: json['id'],
-        username: json['username'],
-      );
 
+  ///sirve para mapear resultados de la api y guardarlo con nombre definidos en esta clase
+  // @JsonKey(name: 'userId')
   final int id;
   final String username;
+
   @override
   List<Object?> get props => [
         id,
         username,
       ];
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 }
